677022eab97d98edaf305235fc0c9dc0f2ba1b0b
20242124 <k0505250@gmail.com>
2026-03-11 22:00:28 +0900

    과제 제출

---
+++ P.53_2.2.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main()
+{
+    printf(". . . . . . . . . . . . . . . . . . . . . . . . \n");
+    printf("  . . . . . . . . . . :-=+=-. .                 \n");
+    printf("  . . . . . . . . . -=#%#%%#=- .                \n");
+    printf(" . . . . . . . . . *%%%%%%%%%%=- .              \n");
+    printf(" . . . . . . . . +%%%%%%%%%%%#+-. .             \n");
+    printf(" . . . . . . . #%%%%%%MM%%%%%%%%%%#*#-. .        \n");
+    printf(" . . . . . . . -M%%%%MM%%%%%%%%%%%#%#+. .        \n");
+    printf(" . . . . . . . #%%%%%#@@%*%@@%####*-. .          \n");
+    printf(" . . . . . . . #%%%%%##**##########+-. .         \n");
+    printf(" . . . . . . . #*#%%%#*:-:+==#*####*-. .         \n");
+    printf(" . . . . . . . -%%%%%#+:- -==::+:+##*-. .        \n");
+    printf(" . . . . . . . -#%#*:-  -==--  -==--=+## .      \n");
+    printf(" . . . . . . . -###- . .  -==--  -=#* .         \n");
+    printf(" . . . . . . . . ##: - - . . . - - -+ .         \n");
+    printf(" . . . . . . . . +*- - - - . - - - -+ .         \n");
+    printf(" . . . . . . . . . - - - - - - - - - . .        \n");
+    printf(" . . . . . . . . . . - - - - - - - . . .        \n");
+    printf(" . . . . . . %%#= - - - - - - - - .#%%+. .       \n");
+    printf(" . . . -%%%%%=- . - - - - - - - . -%%%%#* .      \n");
+    printf(" . . -%%MM%%%%% . . : - - - - - . . +%%%%%%#* .  \n");
+    printf(" . . =%%MM%%%%% . . . - - - - - . . #%%%%%%%#+. .\n");
+    printf(" . . -%%%%MM%%%%% . . . . - - - . . #%%%%%%%%%%#+-\n");
+    printf("MM%%MMMM%%%%%% . . . . . - . . . #%%%%%%%%%%%#++\n");
+    printf("MMMMMMMM%%%%%%- . . . . . - . . . #%%%%%%%%%%%#++\n");
+    printf("MMMMMMMM%%%%%%* . . . . . - . . . #%%%%%%%%%%%#++\n");
+    printf("MMMMMMMMMM%%%% . . . . . . . . #%%%%%%%%%M%%%%%#\n");
+    printf("MMMMMMMMMMMM%%%%: . . . . . . +%%%%%%%%%%%%%%#\n");
+    printf("MMMMWWMMMMMMMM%- . . . . . . +%%%%%%%%%%MM%%%%%\n");
+    printf("MMMMWWMMMMMMMM* . . . . . . #%%%%%%%MMMMMM%%%%%\n");
+    printf("MMMMMMMMMMMMMMM . . . . . . MMM%%%%MMMMMMM%%%%%\n");
+    printf("MMMMWWMMMMMMMM% . . . . . . MMMMMMMMMMMMMMM%%%%\n");
+    printf("WMMMMMMMMMMMMMM- . . . . . MMMMMMMMMMMMMMMMMMM\n");
+    printf("WWMMMMMMMMMMMMMM%% . . . . MMMMMMMMMMMMMMMMMMMM\n");
+    printf("WWWMMMMMMMMMMMMMM* . . . MMMMMMMMMMMMMMMMMMMMM\n");
+
+    return 0;
+}
