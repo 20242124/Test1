6d808bf8833fb1a883a1f36389d175918083616b
20242124 <k0505250@gmail.com>
2026-03-11 21:15:54 +0900

    과제 제출

---
+++ Test.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    printf(" _  _    __  __       _  \n");
+    printf("| |/ /  |  \\/  |     | | \n");
+    printf("| ' /   | |\\/| |     | | \n");
+    printf("| . \\   | |  | |_    | | \n");
+    printf("|_|\\_\\  |_|  |_| \\__/  / \n");
+
+    return 0;
+}
