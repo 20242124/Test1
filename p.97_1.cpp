95e3fd8cc38a11b36fee2cbee0917b7cecbba4b3
20242124 <k0505250@gmail.com>
2026-03-17 11:41:46 +0900

    과제제출

---
+++ p.97_1.cpp
@@ -0,0 +1,15 @@
+#include <iostream> //input output stream
+
+int main()
+{
+    for(int i = 1; i <= 5; ++i) // 초기값은 1. i가 5이하면 1을 계속 더해라.
+    {
+        //std::cout << i << "\n"; //1을 출력하라. \n = 줄을 바꿔라.
+        for(int j = 1; j <= i; ++j)
+        {
+            std::cout << j;
+        }
+        std::cout << "\n";
+    }
+}
+
